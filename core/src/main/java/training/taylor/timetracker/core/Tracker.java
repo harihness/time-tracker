a428daa0e61c6635f0a000244da8d7e43137247f
harihness <110755944+harihness@users.noreply.github.com>
2022-09-09 11:59:01 +0530

    Update Tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -1,7 +1,7 @@
 package training.taylor.timetracker.core;
 
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import training.taylor.timetracker.core.dao.TimeEntry;
 
 import java.util.List;
@@ -40,6 +40,5 @@ public class Tracker {
         }
 
         return entries.get(index);
-        //hari
     }
 }
